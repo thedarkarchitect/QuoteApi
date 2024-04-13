4fca96cf2cc35ff33747d26dae5e217ff463c01e
thedarkarchitect <romanreynolds240@gmail.com>
2024-04-13 15:25:38 +0300

    add cors

--- QuoteApi/index.js
+++ QuoteApi/index.js
@@ -2,13 +2,19 @@ import express from "express";
 import morgan from "morgan";
 import authorRouter from "./routes/author.routes.js";
 import quoteRouter from "./routes/quotes.routes.js";
+import cors from "cors";
 
 const app = express();
-const PORT = 3300;
+const PORT = process.env.PORT || 3300;
 
 //middleware
 app.use(express.json())
 app.use(morgan("dev"))
+app.use(cors({
+    origin: '*', //wildcard is not for production
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true
+}));
 app.use("/authors/", authorRouter);
 app.use("/quotes/", quoteRouter);
 
